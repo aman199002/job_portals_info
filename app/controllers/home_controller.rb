8a67543f74d50b43e1eca5b47140117ab1d08eed
aman <aman@ubuntu.ubuntu-domain>
2013-06-23 14:57:12 +0530

    get details from indeed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,43 +8,17 @@ class HomeController < ApplicationController
   end
 
   def scrape
-    portal = Portal.find_or_create_by_name(params[:url])
-    url = portal.name + '?q=' + params[:keyword].split(' ').join('+') + '&l=' + params[:location].split(' ').join('+')    
-    page = Nokogiri::HTML(open(url))    
-    elements = page.css("[itemtype='http://schema.org/JobPosting']")    
-    @jobs = []    
-    elements.each do |element|
-      begin
-        job = portal.jobs.new
-        job.title = element.at_css('.jobtitle').text.strip
-        job.description = element.at_css('.summary').text.strip
-        job.location = element.at_css('.location').text.strip
-        job.company = element.at_css('.company').text.strip    
-        unless Job.exists?(:title => job.title, :company => job.company, :description => job.description)
-          job.save
-          @jobs << job 
-        end  
-      rescue Exception => e
-        puts "Exception occured: #{e.message}"
-        next
-      end
-    end    
-    @jobs.each_with_index do |job,index|
-      begin
-        linkedin_url = "http://www.linkedin.com/company/" + job.company.downcase.split(' ').join('-')
-        linkedin_page = Nokogiri::HTML(open(linkedin_url))
-        linkedin_description = linkedin_page.css('.container.description p').text.strip
-        size = linkedin_page.xpath("//div[@class='basic-info']//dd[2]").text.strip
-        job.update_attributes(:linkedin_desc => linkedin_description, :size => size)
-        @jobs[index] = job
-      rescue Exception => e
-        puts "Exception occured: #{e.message}"
-        next       
-      end
-    end
+    @portal = Portal.find_or_create_by_name(params[:url])
+    case @portal.name
+    when "http://www.indeed.com/jobs"
+      url = @portal.name + '?q=' + params[:keyword].split(' ').join('+') + '&where=' + params[:location].split(' ').join('+')
+      @jobs = @portal.fetch_all_jobs_for_indeed(url, params[:jobs_count].to_i)
+    when "http://jobsearch.monster.com/search"
+      url = @portal.name + '?q=' + params[:keyword].split(' ').join('+') + '&l=' + params[:location].split(' ').join('+')
+    end      
   end
 
   def all_jobs   
     @jobs = Job.paginate(:page => params[:page], :per_page => 10)    
-  end  
+  end        
 end
--- app/models/portal.rb
+++ app/models/portal.rb
@@ -1,4 +1,63 @@
 class Portal < ActiveRecord::Base
   attr_accessible :name
   has_many :jobs
+
+  
+  def fetch_all_jobs_for_indeed(url, given_jobs_count)
+    @jobs = []
+    page = Nokogiri::HTML(open(url))
+    fetch_jobs_for_indeed(url, page)
+    total_jobs_count = page.css('#searchCount').text.split(" ").last.gsub(',','').to_i
+    available_jobs_count = total_jobs_count > 1000 ? 990 : total_jobs_count
+    required_jobs_count = (given_jobs_count > 0 && given_jobs_count <= available_jobs_count) ? given_jobs_count : available_jobs_count    
+    1.upto(required_jobs_count/10) do |count|      
+      fetch_jobs_for_indeed(url.concat("&start=#{count*10}"))
+    end
+    @jobs.flatten
+  end	
+
+
+  def fetch_jobs_for_indeed(url, page=nil)  	
+    page = Nokogiri::HTML(open(url)) if page.nil?
+	elements = page.css("[itemtype='http://schema.org/JobPosting']")    
+	jobs = []	
+	elements.each do |element|
+	  begin
+	    job = self.jobs.new
+	  	job.title = element.at_css('.jobtitle').text.strip
+	  	job.description = element.at_css('.summary').text.strip
+	  	job.location = element.at_css('.location').text.strip
+	  	job.company = element.at_css('.company').text.strip    
+	  	unless Job.exists?(:title => job.title, :company => job.company, :description => job.description)
+	  	  job.save
+	  	  jobs << job 
+	  	end
+	  rescue Exception => e
+	    puts "Exception occured: #{e.message}"
+	  	next
+      end
+	end    
+	get_info_from_linkedin(jobs) if jobs.present?
+	@jobs << jobs
+  end
+
+  private
+
+    def get_info_from_linkedin(jobs)
+	  jobs.each_with_index do |job,index|
+	  begin
+	    linkedin_url = "http://www.linkedin.com/company/" + job.company.downcase.split(' ').join('-')	
+	    linkedin_page = Nokogiri::HTML(open(linkedin_url))
+	    linkedin_description = linkedin_page.css('.container.description p').text.strip
+	    size = linkedin_page.xpath("//div[@class='basic-info']//dd[2]").text.strip
+	    job.update_attributes(:linkedin_desc => linkedin_description, :size => size)
+	    jobs[index] = job
+	  rescue Exception => e
+	    puts "Exception occured: #{e.message}"
+	    next       
+      end
+    end	
+
+  end  
+
 end
